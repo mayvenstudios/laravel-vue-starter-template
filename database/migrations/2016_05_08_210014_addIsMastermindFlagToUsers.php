12e23a84a1f01075c424ed0feec8b1125af58301
Buzdyk Gregory <buzdyk.gregory@gmail.com>
2016-05-08 21:05:36 +0000

    commit forgotten files

---
+++ database/migrations/2016_05_08_210014_addIsMastermindFlagToUsers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsMastermindFlagToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->boolean('is_mastermind')->default(false)->after('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            $table->dropColumn('is_mastermind');
+        });
+    }
+}
